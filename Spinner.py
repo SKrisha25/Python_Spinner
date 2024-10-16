66975f64e7e216a20512740b31fb9ae009f36df1
Krisha Sardhara <sardharakrisha3020@gmail.com>
2024-10-16 22:54:18 +0530

    Add files via upload

---
+++ Spinner.py
@@ -0,0 +1,11 @@
+import turtle 
+
+colours = ['red' , 'purple' , 'blue' , 'green' , 'white' , 'pink']
+t = turtle.Pen()
+turtle.bgcolor("black")
+t.speed(10)
+for x in range(360):
+    t.pencolor(colours[x % 6])
+    t.width(x//100 + 2)
+    t.forward(x)
+    t.left(59)
